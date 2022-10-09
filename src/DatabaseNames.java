8fb6d6b67e01d19bebccae2058cd8004f36a9594
Stan <stasostaszewski@gmail.com>
2022-10-09 13:21:13 +0200

    names

---
+++ src/DatabaseNames.java
@@ -0,0 +1,27 @@
+
+public class DatabaseNames {
+    public static class Tables {
+        public static String ingredients = "Ingredients";
+        public static String menuItems = "MenuItems";
+        public static String orders = "Orders";
+        public static String clients = "Clients";
+        public static String codes = "Codes";
+        public static String couriers = "Couriers";
+    }
+    public static class IngredientKeys{
+        public static String ingredientID = "ingredientID";
+
+    }
+    public static class MenuItemsKeys{
+        public static String ingredientID = "ingredientID";
+
+    }
+    public static class OrderKeys{
+        public static String ingredientID = "ingredientID";
+
+    }
+    public static class ClientKeys{
+        public static String ingredientID = "ingredientID";
+
+    }
+}
